7d07027e07959c64f2e737650d321a382b7a4f03
Mateusz <msikorski@griddynamics.com>
2024-09-24 10:59:03 +0200

    Implement message listener with manual acks.

---
+++ user-service/src/main/java/mat_sik/user_service/client/rabbit/listener/RabbitListenerContainerConfiguration.java
@@ -0,0 +1,39 @@
+package mat_sik.user_service.client.rabbit.listener;
+
+import mat_sik.user_service.user.controller.CreateUserMessageListener;
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+
+@Configuration
+public class RabbitListenerContainerConfiguration {
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer createUserListenerContainer(
+            ConnectionFactory factory,
+            ExecutorService executorService,
+            @Qualifier("createUserQueue") Queue queue,
+            CreateUserMessageListener messageListener
+    ) {
+        var container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(factory);
+        container.setTaskExecutor(executorService);
+        container.setQueues(queue);
+        container.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        container.setMessageListener(messageListener);
+        return container;
+    }
+
+}
---
+++ user-service/src/main/java/mat_sik/user_service/user/controller/CreateUserMessageListener.java
@@ -0,0 +1,42 @@
+package mat_sik.user_service.user.controller;
+
+import com.rabbitmq.client.Channel;
+import lombok.RequiredArgsConstructor;
+import mat_sik.user_service.user.controller.message.CreateUserMessage;
+import mat_sik.user_service.user.model.User;
+import mat_sik.user_service.user.service.UserService;
+import org.bson.types.ObjectId;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CreateUserMessageListener implements ChannelAwareMessageListener {
+
+    private static final boolean MULTIPLE_ACK = false;
+
+    private final UserService service;
+    private final Jackson2JsonMessageConverter converter;
+
+    @Override
+    public void onMessage(Message message, Channel channel) throws IOException {
+        MessageProperties messageProperties = message.getMessageProperties();
+        long deliveryTag = messageProperties.getDeliveryTag();
+
+        var createUserMessage = (CreateUserMessage) converter.fromMessage(message, CreateUserMessage.class);
+
+        ObjectId id = createUserMessage.id();
+        String firstname = createUserMessage.firstname();
+        String lastname = createUserMessage.lastname();
+
+        service.createUser(new User(id, firstname, lastname));
+
+        channel.basicAck(deliveryTag, MULTIPLE_ACK);
+    }
+
+}
