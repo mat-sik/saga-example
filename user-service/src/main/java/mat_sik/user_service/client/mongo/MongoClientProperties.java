262e672296071141fa758a96e1e88e7ed1062c57
Mateusz <msikorski@griddynamics.com>
2024-09-24 10:57:37 +0200

    Improve properties formating.

--- user-service/src/main/java/mat_sik/user_service/client/mongo/MongoClientProperties.java
+++ user-service/src/main/java/mat_sik/user_service/client/mongo/MongoClientProperties.java
@@ -3,5 +3,11 @@ package mat_sik.user_service.client.mongo;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("mongo")
-public record MongoClientProperties(String host, int port, String login, char[] password, String authDatabase, String database) {
+public record MongoClientProperties(
+        String host,
+        int port,
+        String login,
+        char[] password,
+        String authDatabase,
+        String database) {
 }
