be6d213e1dedb5b993f08746792a661c70ce2be4
Mateusz <msikorski@griddynamics.com>
2024-09-25 09:38:04 +0200

    Create initial project for saga orchestrator.

---
+++ saga-orchestrator/src/main/java/mat_sik/saga_orchestrator/SagaOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package mat_sik.saga_orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaOrchestratorApplication.class, args);
+	}
+
+}
---
+++ saga-orchestrator/src/test/java/mat_sik/saga_orchestrator/SagaOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package mat_sik.saga_orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
