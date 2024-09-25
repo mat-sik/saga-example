56f0df6b17b76b2de9a943afe9a4c778a52c40ce
Mateusz <msikorski@griddynamics.com>
2024-09-25 10:04:52 +0200

    Add messages for user creation.

---
+++ saga-orchestrator/src/main/java/mat_sik/saga_orchestrator/user/controller/create/CreateUserMessage.java
@@ -0,0 +1,13 @@
+package mat_sik.saga_orchestrator.user.controller.create;
+
+import org.bson.types.ObjectId;
+
+public record CreateUserMessage(
+        ObjectId id,
+        String firstname,
+        String lastname,
+        String username,
+        String email,
+        String password
+) {
+}
